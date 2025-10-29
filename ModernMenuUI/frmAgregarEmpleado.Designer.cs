c0c5a56018952755453e6f6f4b904f7f70b44005
Fernando Jose Barahona Castro <fbarahona280@gmail.com>
2025-10-28 22:13:00 -0600

    frmEmpleados

---
+++ ModernMenuUI/frmAgregarEmpleado.Designer.cs
@@ -0,0 +1,412 @@
+﻿namespace ModernMenuUI
+{
+    partial class frmAgregarEmpleado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAgregarEmpleado));
+            Panel panBarraControl;
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label9 = new Label();
+            label8 = new Label();
+            txtCodigo = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            txtProducto = new TextBox();
+            txtPrecio = new TextBox();
+            radioButton1 = new RadioButton();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            groupBox1 = new GroupBox();
+            radioButton2 = new RadioButton();
+            btnAgregar = new Button();
+            panel2 = new Panel();
+            btnAjustes = new Button();
+            btnCerrar = new Button();
+            lblNombreModulo = new Label();
+            panel1 = new Panel();
+            panBarraControl = new Panel();
+            groupBox1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            panBarraControl.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Itim", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.FromArgb(87, 99, 110);
+            label6.Location = new Point(14, 199);
+            label6.Name = "label6";
+            label6.Size = new Size(57, 18);
+            label6.TabIndex = 27;
+            label6.Text = "Correo:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Itim", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.FromArgb(87, 99, 110);
+            label5.Location = new Point(14, 284);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 18);
+            label5.TabIndex = 26;
+            label5.Text = "Estado:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Itim", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(87, 99, 110);
+            label4.Location = new Point(14, 163);
+            label4.Name = "label4";
+            label4.Size = new Size(68, 18);
+            label4.TabIndex = 25;
+            label4.Text = "Teléfono:";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.White;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Enabled = false;
+            textBox2.Font = new Font("Itim", 13F);
+            textBox2.Location = new Point(116, 196);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(315, 21);
+            textBox2.TabIndex = 23;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.White;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Enabled = false;
+            textBox1.Font = new Font("Itim", 13F);
+            textBox1.Location = new Point(116, 160);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(315, 21);
+            textBox1.TabIndex = 22;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Comic Sans MS", 12F);
+            label9.ForeColor = Color.FromArgb(87, 99, 110);
+            label9.Location = new Point(10, 9);
+            label9.Name = "label9";
+            label9.Size = new Size(153, 23);
+            label9.TabIndex = 20;
+            label9.Text = "Datos del Producto:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Itim", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.FromArgb(87, 99, 110);
+            label8.Location = new Point(12, 49);
+            label8.Name = "label8";
+            label8.Size = new Size(38, 18);
+            label8.TabIndex = 19;
+            label8.Text = "DNI:";
+            // 
+            // txtCodigo
+            // 
+            txtCodigo.BackColor = Color.White;
+            txtCodigo.BorderStyle = BorderStyle.None;
+            txtCodigo.Enabled = false;
+            txtCodigo.Font = new Font("Itim", 13F);
+            txtCodigo.Location = new Point(116, 46);
+            txtCodigo.Name = "txtCodigo";
+            txtCodigo.ReadOnly = true;
+            txtCodigo.Size = new Size(315, 21);
+            txtCodigo.TabIndex = 18;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Itim", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(87, 99, 110);
+            label2.Location = new Point(13, 126);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 18);
+            label2.TabIndex = 16;
+            label2.Text = "Apellido:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Itim", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(87, 99, 110);
+            label1.Location = new Point(12, 90);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 18);
+            label1.TabIndex = 15;
+            label1.Text = "Nombre:";
+            // 
+            // txtProducto
+            // 
+            txtProducto.BackColor = Color.White;
+            txtProducto.BorderStyle = BorderStyle.None;
+            txtProducto.Enabled = false;
+            txtProducto.Font = new Font("Itim", 13F);
+            txtProducto.Location = new Point(116, 87);
+            txtProducto.Name = "txtProducto";
+            txtProducto.ReadOnly = true;
+            txtProducto.Size = new Size(315, 21);
+            txtProducto.TabIndex = 13;
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.BackColor = Color.White;
+            txtPrecio.BorderStyle = BorderStyle.None;
+            txtPrecio.Enabled = false;
+            txtPrecio.Font = new Font("Itim", 13F);
+            txtPrecio.Location = new Point(116, 123);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.ReadOnly = true;
+            txtPrecio.Size = new Size(315, 21);
+            txtPrecio.TabIndex = 12;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(8, 22);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(59, 19);
+            radioButton1.TabIndex = 28;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Activo";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Itim", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(87, 99, 110);
+            label3.Location = new Point(14, 238);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 18);
+            label3.TabIndex = 32;
+            label3.Text = "Presentación:";
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.White;
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Enabled = false;
+            textBox3.Font = new Font("Itim", 13F);
+            textBox3.Location = new Point(116, 235);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(315, 21);
+            textBox3.TabIndex = 31;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(116, 262);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(315, 54);
+            groupBox1.TabIndex = 30;
+            groupBox1.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(124, 21);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(67, 19);
+            radioButton2.TabIndex = 29;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Inactivo";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.BackColor = Color.FromArgb(149, 195, 172);
+            btnAgregar.BackgroundImage = (Image)resources.GetObject("btnAgregar.BackgroundImage");
+            btnAgregar.BackgroundImageLayout = ImageLayout.None;
+            btnAgregar.FlatAppearance.BorderSize = 0;
+            btnAgregar.Font = new Font("Itim", 18.25F);
+            btnAgregar.ForeColor = SystemColors.ButtonFace;
+            btnAgregar.ImageAlign = ContentAlignment.BottomLeft;
+            btnAgregar.Location = new Point(108, 383);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Padding = new Padding(40, 0, 0, 0);
+            btnAgregar.Size = new Size(225, 46);
+            btnAgregar.TabIndex = 10;
+            btnAgregar.Text = "Guardar";
+            btnAgregar.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(189, 215, 238);
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(groupBox1);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(txtCodigo);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(txtProducto);
+            panel2.Controls.Add(txtPrecio);
+            panel2.Controls.Add(btnAgregar);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 62);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(508, 388);
+            panel2.TabIndex = 14;
+            // 
+            // btnAjustes
+            // 
+            btnAjustes.BackColor = Color.FromArgb(148, 168, 187);
+            btnAjustes.Dock = DockStyle.Left;
+            btnAjustes.FlatAppearance.BorderSize = 0;
+            btnAjustes.FlatStyle = FlatStyle.Flat;
+            btnAjustes.Font = new Font("Century Gothic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAjustes.ForeColor = Color.FromArgb(167, 191, 211);
+            btnAjustes.Image = Properties.Resources.editar__1_;
+            btnAjustes.ImageAlign = ContentAlignment.MiddleLeft;
+            btnAjustes.Location = new Point(0, 0);
+            btnAjustes.Name = "btnAjustes";
+            btnAjustes.Padding = new Padding(11, 0, 0, 0);
+            btnAjustes.Size = new Size(65, 62);
+            btnAjustes.TabIndex = 7;
+            btnAjustes.TextAlign = ContentAlignment.MiddleLeft;
+            btnAjustes.UseVisualStyleBackColor = false;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.BackColor = Color.FromArgb(148, 168, 187);
+            btnCerrar.Dock = DockStyle.Right;
+            btnCerrar.FlatAppearance.BorderSize = 0;
+            btnCerrar.FlatStyle = FlatStyle.Flat;
+            btnCerrar.Font = new Font("Century Gothic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCerrar.ForeColor = Color.FromArgb(167, 191, 211);
+            btnCerrar.Image = (Image)resources.GetObject("btnCerrar.Image");
+            btnCerrar.ImageAlign = ContentAlignment.MiddleLeft;
+            btnCerrar.Location = new Point(443, 0);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Padding = new Padding(11, 0, 0, 0);
+            btnCerrar.Size = new Size(65, 62);
+            btnCerrar.TabIndex = 3;
+            btnCerrar.TextAlign = ContentAlignment.MiddleLeft;
+            btnCerrar.UseVisualStyleBackColor = false;
+            // 
+            // lblNombreModulo
+            // 
+            lblNombreModulo.AutoSize = true;
+            lblNombreModulo.Font = new Font("Itim", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblNombreModulo.ForeColor = Color.White;
+            lblNombreModulo.Location = new Point(6, 10);
+            lblNombreModulo.Name = "lblNombreModulo";
+            lblNombreModulo.Size = new Size(219, 29);
+            lblNombreModulo.TabIndex = 8;
+            lblNombreModulo.Text = "EDITAR PRODUCTO";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(lblNombreModulo);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(65, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(218, 62);
+            panel1.TabIndex = 9;
+            // 
+            // panBarraControl
+            // 
+            panBarraControl.BackColor = Color.FromArgb(148, 168, 187);
+            panBarraControl.Controls.Add(panel1);
+            panBarraControl.Controls.Add(btnAjustes);
+            panBarraControl.Controls.Add(btnCerrar);
+            panBarraControl.Dock = DockStyle.Top;
+            panBarraControl.ForeColor = Color.Coral;
+            panBarraControl.Location = new Point(0, 0);
+            panBarraControl.Name = "panBarraControl";
+            panBarraControl.Size = new Size(508, 62);
+            panBarraControl.TabIndex = 13;
+            // 
+            // frmAgregarEmpleado
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(508, 450);
+            Controls.Add(panel2);
+            Controls.Add(panBarraControl);
+            Name = "frmAgregarEmpleado";
+            Text = "frmAgregarEmpleado";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panBarraControl.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label9;
+        private Label label8;
+        private TextBox txtCodigo;
+        private Label label2;
+        private Label label1;
+        private TextBox txtProducto;
+        private TextBox txtPrecio;
+        private RadioButton radioButton1;
+        private Label label3;
+        private TextBox textBox3;
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private Button btnAgregar;
+        private Panel panel2;
+        private Button btnAjustes;
+        private Button btnCerrar;
+        public Label lblNombreModulo;
+        private Panel panel1;
+    }
+}
