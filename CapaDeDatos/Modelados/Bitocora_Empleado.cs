6640a34ae3b771d3582f4480cc5d9dd64e57b33d
warthunderlover <lazoemanuel29@gmail.com>
2025-11-02 20:50:23 -0600

    agregando bitacora

---
+++ CapaDeDatos/Modelados/Bitocora_Empleado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDeDatos.Modelados
+{
+    internal class Bitocora_Empleado
+    {
+        public class BitacoraEmpleado
+        {
+            public int Id_Bitacora { get; set; }
+            public int Id_Empleado { get; set; }
+            public string Estado_Anterior { get; set; }
+            public string Estado_Actual { get; set; }
+            public string Campo_Afectado { get; set; }
+            public DateTime Fecha_Hora { get; set; }
+            public string Campo_Extra { get; set; }
+            public int Id_Accion { get; set; }
+            public int Id_Modulo { get; set; }
+        }
+    }
+}
