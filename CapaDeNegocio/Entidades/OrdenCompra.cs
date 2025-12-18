79b9a65071691e5bf8ff600e3bb189ffec351b03
Fernando <fbarahona@gmail.com>
2025-12-17 23:34:58 -0600

    su

--- CapaDeNegocio/Entidades/OrdenCompra.cs
+++ CapaDeNegocio/Entidades/OrdenCompra.cs
@@ -12,8 +12,6 @@ namespace CapaDeNegocio.Entidades
         public string Producto { get; set; }
         public decimal Precio { get; set; }
         public int Cantidad { get; set; }
-
-        // El reporte puede calcular esto, o lo puedes pasar tú
         public decimal TotalLinea => Precio * Cantidad;
     }
 }
